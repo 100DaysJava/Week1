5f3488c74ebf445cc93644177b53b899353f0fbc
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2020-08-27 22:54:24 +0530

    Code 21 Day 7

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+package Baisc_Prgs;
+import java.util.*;
+public class Factorial 
+{
+	public static void main(String[] args) 
+	{
+	   Scanner obj = new Scanner(System.in);
+	   System.out.println("enter number:");
+	   int n = obj.nextInt(); // 5
+	   int f =1;
+	   for(int i = 1; i<=n ; i++)
+	   {
+		   f = f*i;
+	   }
+	   System.out.println("Factorial = "+f);
+	} }
